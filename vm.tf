083c3c54954bd8009c40e8f0db89367c0522b28e
Julio Ribeiro <jribeiro@kroton.tech>
2022-05-07 21:27:37 -0300

    git actions

--- vm.tf
+++ vm.tf
@@ -9,8 +9,8 @@ resource "aws_instance" "vm" {
   ami                         = "ami-0eea504f45ef7a8f7"
   instance_type               = "t3.medium"
   key_name                    = aws_key_pair.key.key_name
-  subnet_id                   = aws_subnet.subnet.0.subnet_id
-  vpc_security_group_ids      = aws_security_group.security_group.security_group_id
+  subnet_id                   = aws_subnet.subnet.0.id
+  vpc_security_group_ids      = aws_security_group.security_group.id
   associate_public_ip_address = true
 
   tags = {
