ef5b7455efca85803ed23b18c0a3901192d08f96
Júlio Ribeiro <52141340+juliioribeiro@users.noreply.github.com>
2022-05-07 21:00:42 -0300

    Delete 58.CI directory

--- 58.CI/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "subnet_id" {
-  value = aws_subnet.subnet[*].id
-}
-
-output "vm_ip" {
-  value = aws_instance.vm.public_ip
-}
--- 58.CI/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "portas" {
-  description = "Portas do security group"
-  type        = list(number)
-  default     = [22, 80, 443, 8080]
-}
