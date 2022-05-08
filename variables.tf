90da44213eaebf66bdc5570d68424c6cc5dca72d
Julio Ribeiro <jribeiro@kroton.tech>
2022-05-07 21:12:47 -0300

    git actions

--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "portas" {
   type        = list(number)
   default     = [22, 80, 443, 8080]
 }
+
+variable "aws_pub_key" {
+  description = "Public key para VM na AWS"
+  type        = string
+}
